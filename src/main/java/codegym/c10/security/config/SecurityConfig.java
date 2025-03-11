38fb5def53176296919929efd5dc64cac9a44f8b
crew52 <dungnc150602@gmail.com>
2025-03-11 08:18:11 +0700

    update BCryptPasswordEncoder

--- src/main/java/codegym/c10/security/config/SecurityConfig.java
+++ src/main/java/codegym/c10/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -28,12 +29,13 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails user = User.withUsername("dung").password("dung").roles("USER").build();
+        UserDetails user = User.withUsername("dung").password("$2a$12$bN172N/EcowoNYC3k4D7lOuY2Bg322IpF9Dp5UJDFhOJjckUv/X0e").roles("USER").build();
         UserDetails admin = User.withUsername("dung1").password("dung1").roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
